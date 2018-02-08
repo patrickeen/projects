e2a10bc64c4d6195c39ff47a159e4c2cd3d0b530
patrickeen <patrick.anre@gmail.com>
2018-02-08 20:33:38 +0100

    js changes

--- coffee-shop/js/main.js
+++ coffee-shop/js/main.js
@@ -1,17 +1,45 @@
 $(function(){
 
 	var $orders = $('#orders');
-
+	var $name = $('#name');
+	var $drink = $('#drink');
 	$.ajax({
 		type: 'GET',
 		//url: 'https://learnwebcode.github.io/json-example/animals-1.json',
-		url: 'https://raw.githubusercontent.com/patrickeen/projects/master/coffee-shop/api/orders.json',
+		//url: 'https://raw.githubusercontent.com/patrickeen/projects/master/coffee-shop/api/orders.json',
+		url: 'api/orders.json',
 		success: function(orders){
 			$.each(orders,function(i,order){
-				$orders.append('<li>my order</li>');
+				$orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
 			});
-		console.log('success',orders);
+		},
+		error: function(){
+			alert('error loading orders');
 		}
+
 	});
+	
+	//adding new orders
+	$('#add-order').on('click',function(){
+
+		var order = {
+			name: $name.val(),
+			drink: $drink.val(),
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: 'api/orders.json',
+			data: order,
+			success: function(newOrder){
+				$orders.append('<li>name: ' + newOrder.name + ', drink: ' + newOrder.drink + '</li>');
+			},
+			error: function(){
+				alert('error saving order');
+			}
+		});
+
+	});
+	
 
 });
