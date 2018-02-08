579445769b8493e51aa997bcfbab5a9244e6d7ac
patrickeen <patrick.anre@gmail.com>
2018-02-08 16:40:46 +0100

    js and json updates

--- coffee-shop/js/main.js
+++ coffee-shop/js/main.js
@@ -4,10 +4,11 @@ $(function(){
 
 	$.ajax({
 		type: 'GET',
+		//url: 'https://learnwebcode.github.io/json-example/animals-1.json',
 		url: 'https://raw.githubusercontent.com/patrickeen/projects/master/coffee-shop/api/orders.json',
 		success: function(orders){
-			$.each(orders, function(i, order){
-				$orders.append('<li>my order</li>')
+			$.each(orders,function(i,order){
+				$orders.append('<li>my order</li>');
 			});
 		}
 	});
